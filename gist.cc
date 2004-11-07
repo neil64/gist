d255e8abe48b920bea544500d9695ebe44284b92
Neil Russell <neil64@gmail.com>
2004-11-06 21:52:20 -0800

    gist: Talk about the garbage collector in the man page.
    *  Talk about the garbage collector in the man page.
    *  Explicitly initialize the garbage collector, so we can clear
       the "GC_all_interior_pointers" flag.
    
    -- p4 change 888

--- gist.cc
+++ gist.cc
@@ -16,14 +16,41 @@ gist::gistInternal	gist::Int = { GT_INT };
 gist::gistInternal	gist::Float = { GT_FLOAT };
 
 /**********************************************************************/
-
 /*
  *	Memory allocation.
  */
 
+
+static int	gcInitialized = 0;
+
+/*
+ *	Initialize the garbage collector if needed.
+ */
+static inline void
+gcInit()
+{
+	if (!gcInitialized)
+	{
+		extern int GC_is_initialized;
+		if (!GC_is_initialized)
+		{
+			GC_all_interior_pointers = 0;
+			GC_init();
+		}
+
+		gist x(0);
+
+		gcInitialized = x.ty;
+		// gcInitialized = 1;
+	}
+}
+
+
 void *
 gist::operator new(unsigned sz)
 {
+	gcInit();
+
 	/*
 	 *	Allocate it through the Boehm garbage collector.
 	 *	The allocation size here should almost always be 8 (or
@@ -40,6 +67,8 @@ gist::operator new(unsigned sz)
 void *
 giBase::operator new(unsigned sz)
 {
+	gcInit();
+
 	void * ptr = GC_malloc(sz);
 	if (!ptr)
 		throw std::bad_alloc();
